12846a28646ec7f14e63df0f25bee2e207eeaecf
manuelsansoresg <manuelsansoresg@gmail.com>
2026-07-05 22:32:41 -0600

    intro invitacion

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Plantillas de invitación (preview). Más adelante se resuelven
+// por slug desde un controlador + modelo Invitacion.
+Route::view('/invitacion/xv-valeria', 'templates.xv-valeria');
