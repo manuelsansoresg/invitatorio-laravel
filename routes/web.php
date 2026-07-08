2372f3027303292341e312d2d0bbb75e15eeb745
manuelsansoresg <manuelsansoresg@gmail.com>
2026-07-08 15:57:09 -0600

    feat: add xv valeria 15th birthday invitation page
    Update web route to use the correct view path, add custom pink-themed CSS and interactive scripts, and include all required static assets like images, web fonts, and audio files.

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,4 @@ Route::get('/', function () {
 
 // Plantillas de invitación (preview). Más adelante se resuelven
 // por slug desde un controlador + modelo Invitacion.
-Route::view('/invitacion/xv-valeria', 'templates.xv-valeria');
+Route::view('/invitacion/xv-valeria', 'invitaciones.xv.valeria');
